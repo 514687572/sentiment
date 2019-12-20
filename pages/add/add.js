4c884ba1cb86e68e9f5203b2ddafb020ab1770c9
514687572@qq.com <chenjunan@sm.vvip-u.com>
2019-12-20 17:30:51 +0800

    保存
    保存

--- pages/add/add.js
+++ pages/add/add.js
@@ -18,10 +18,7 @@ Page({
       wx.request({
         url: glb.serverUrl + '/sub/addSubscribe', //仅为示例，并非真实的接口地址
         method:'POST',
-        data: {
-          subUrl: this.data.weboUrl,
-          userOpenId: JSON.parse(wx.getStorageSync('userKey')).sessionKey.split("&$&")[1]
-        },
+        data: sub,
         header: {
           'content-type': 'application/json' // 默认值
         },
